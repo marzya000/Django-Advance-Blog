e84482c37638a78a979bf563d5f6d3c646fac286
Marzya <mnaq_1987@yahoo.com>
2025-10-10 21:09:39 +0330

    A deeper look at RedirectView
    changed blog urls and views.
    We were able to redirect to the Maktabkhoneh site using the RedirectView class and some changes to our URL in the blog app's urls.py.

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 from .models import Post
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
+# Function Base View show a template
+'''
 def indexView(request):
     """
     a function based view to show index page
@@ -9,6 +13,7 @@ def indexView(request):
     name = 'marzya'
     context = {'name':name}
     return render(request,"index.html",context)
+'''
 
 class IndexView(TemplateView):
     """
@@ -21,3 +26,18 @@ class IndexView(TemplateView):
         context['name'] = 'marzya'
         context['posts'] = Post.objects.all()
         return context
+    
+''' FBV for redirect
+
+from django.shortcuts import redirect
+def RedirectToMaktab(request):
+    return redirect('https://maktabkhooneh.com')
+
+'''
+class RedirectToMaktab(RedirectView):
+    url ='https://maktabkhooneh.org/'
+
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
