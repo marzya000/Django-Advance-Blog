42c78fa45745bd17fcdc30d86f1985bff03090f2
Marzya <mnaq_1987@yahoo.com>
2025-10-10 19:11:37 +0330

    Introducing RedirectView
    Introducing RedirectView.
    we created a path in blog urls by RedirectView  and pattern_name,.....

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
-
+app_name = "blog"
 urlpatterns = [
     path('fbv-index', views.indexView,name="fbv-index"),
     #path('cbv-index', TemplateView.as_view(template_name="index.html", extra_context={'name':'marzya'})),
     path('cbv-index', views.IndexView.as_view(),name='cbv-index'),
+    path('go-to-index', RedirectView.as_view(pattern_name='blog:cbv-index'), name='redirect-to-index'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -11,10 +11,11 @@ def indexView(request):
     return render(request,"index.html",context)
 
 class IndexView(TemplateView):
-     """
+    """
     a class based view to show index page
     """
     template_name = 'index.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['name'] = 'marzya'
