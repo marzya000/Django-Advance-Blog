e353a9f3761138495c469d6e2823c6fd841913cb
Marzya <mnaq_1987@yahoo.com>
2025-10-11 13:04:50 +0330

    Introducing ListView
    Introducing ListView

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -6,5 +6,7 @@ from django.views.generic.base import RedirectView
 app_name = "blog"
 urlpatterns = [
     path('cbv-index', views.IndexView.as_view(),name='cbv-index'),
+    path('post/', views.PostList.as_view(), name='post-list'),
     path('go-to-maktabkhooneh/<int:pk>/', views.RedirectToMaktab.as_view(),name='redirect-to-maktabkhooneh'),
+    
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import ListView
 from .models import Post
 from django.shortcuts import get_object_or_404
 # Create your views here.
@@ -41,3 +42,13 @@ class RedirectToMaktab(RedirectView):
         post = get_object_or_404(Post, pk=kwargs['pk'])
         print(post)
         return super().get_redirect_url(*args, **kwargs)
+    
+
+class PostList(ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        posts = Post.objects.filter(status=False)
+        return posts
+    
