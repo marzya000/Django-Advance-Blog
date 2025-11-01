52ef7a5f525fdc3cc7c972d66bfe1e98b3f507ab
Marzya <mnaq_1987@yahoo.com>
2025-11-01 21:50:46 +0330

    Update views.py
    Updating post information via Put

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -20,11 +20,17 @@ def postList(request):
         return Response(serializer.data)
        
 
-@api_view()
+@api_view(["GET","PUT"])
 def postDetail(request,id):
     post = get_object_or_404(Post,pk=id,status=True)
-    serializer=PostSerializer(post)
-    return Response(serializer.data)
+    if request.method == "GET":        
+        serializer=PostSerializer(post)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = PostSerializer(post,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
    
 
 
