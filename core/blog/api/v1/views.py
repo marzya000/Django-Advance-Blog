44cd7946c7f77c4596a8af0c3336a5c69cee1374
Marzya <mnaq_1987@yahoo.com>
2025-11-01 13:04:33 +0330

    Using serializer to get list of posts
    Using serializer to get list of posts

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -9,11 +9,13 @@ from django.shortcuts import get_object_or_404
 
 @api_view()
 def postList(request):
-    return Response('ok')
+    posts = Post.objects.filter(status=True)
+    serializer = PostSerializer(posts,many=True)
+    return Response(serializer.data)
 
 @api_view()
 def postDetail(request,id):
-    post = get_object_or_404(Post,pk=id)
+    post = get_object_or_404(Post,pk=id,status=True)
     serializer=PostSerializer(post)
     return Response(serializer.data)
    
