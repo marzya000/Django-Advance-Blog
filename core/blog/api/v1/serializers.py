cb8cef6de9feb01d9ad69495c86aca8ad0af5e3f
Marzya <mnaq_1987@yahoo.com>
2025-11-05 21:06:02 +0330

    Update serializers.py
    ReadOnly or not?

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -8,10 +8,11 @@ from ...models import Post, Category
 
 
 class PostSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Post
         fields = ['id','author','title','content','status','category','created_date','published_date']
-
+       
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
