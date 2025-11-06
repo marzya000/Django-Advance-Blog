a065bf2eedfecdda514efee8463928460179c87c
Marzya <mnaq_1987@yahoo.com>
2025-11-06 16:12:18 +0330

    Update serializers.py
    Types of data dependencies to serializer and to_representation

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -7,10 +7,18 @@ from ...models import Post, Category
     #title = serializers.CharField(max_length=255)
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id','name']
+
+
+
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url',read_only=True)
     absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
+
     class Meta:
         model = Post
         fields = ['id','author','title','content','snippet','status','relative_url','absolute_url','category','created_date','published_date']
@@ -18,9 +26,8 @@ class PostSerializer(serializers.ModelSerializer):
     def get_abs_url(self,obj):
         request = self.context.get('request')
         return request.build_absolute_uri(obj.pk)
-      
+    
+    def to_representation(self,instance):
+        return super().to_representation(instance)
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id','name']
+      
