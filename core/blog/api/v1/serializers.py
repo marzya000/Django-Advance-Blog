e266eaec1ee7a643d2bae6c610605782dbfc9410
Marzya <mnaq_1987@yahoo.com>
2025-11-06 20:53:31 +0330

    Update serializers.py
    Getting user information from the request and the importance of the request in the Serializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers # type: ignore
 from ...models import Post, Category
+from accounts.models import Profile
 
 
 #class PostSerializer(serializers.Serializer):
@@ -11,9 +12,8 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id','name']
-
-
-
+    
+    
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url',read_only=True)
@@ -22,6 +22,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id','author','image','title','content','snippet','status','relative_url','absolute_url','category','created_date','published_date']
+        read_only_fields = ['author']
     
     def get_abs_url(self,obj):
         request = self.context.get('request')
@@ -36,7 +37,11 @@ class PostSerializer(serializers.ModelSerializer):
             rep.pop('absolute_url',None)
         else:
             rep.pop('content',None)
-        rep['category'] = CategorySerializer(instance.category).data    
+        rep['category'] = CategorySerializer(instance.category,context={'request':request}).data    
         return rep
+    
+    def create(self, validated_data):      
+        validated_data['author'] = Profile.objects.get(user__id = self.context.get('request').user.id)
+        return super().create(validated_data)
 
       
