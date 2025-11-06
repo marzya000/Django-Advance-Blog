31c4217abaeaea8bf457cd252ba1092b92394983
Marzya <mnaq_1987@yahoo.com>
2025-11-06 14:54:17 +0330

    Update serializers.py
    Functions related to internal serializer requests

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -10,10 +10,15 @@ from ...models import Post, Category
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url',read_only=True)
+    absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
     class Meta:
         model = Post
-        fields = ['id','author','title','content','snippet','status','relative_url','category','created_date','published_date']
-       
+        fields = ['id','author','title','content','snippet','status','relative_url','absolute_url','category','created_date','published_date']
+    
+    def get_abs_url(self,obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
+      
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
