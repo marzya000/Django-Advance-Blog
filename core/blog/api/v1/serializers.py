c60edd39171cdce1967c2f94508c68bb5ca12e3d
Marzya <mnaq_1987@yahoo.com>
2025-11-06 14:19:12 +0330

    Model-dependent functions
    Model-dependent functions

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -8,10 +8,11 @@ from ...models import Post, Category
 
 
 class PostSerializer(serializers.ModelSerializer):
-    
+    snippet = serializers.ReadOnlyField(source='get_snippet')
+    relative_url = serializers.URLField(source='get_absolute_api_url',read_only=True)
     class Meta:
         model = Post
-        fields = ['id','author','title','content','status','category','created_date','published_date']
+        fields = ['id','author','title','content','snippet','status','relative_url','category','created_date','published_date']
        
 
 class CategorySerializer(serializers.ModelSerializer):
