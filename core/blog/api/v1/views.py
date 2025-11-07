4a9f8ba142f85eacf058cf23a0f8ca58c9352667
Marzya <mnaq_1987@yahoo.com>
2025-11-07 20:10:35 +0330

    Update views.py
    More filters

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -114,7 +114,7 @@ class PostModelViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
     filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
-    filterset_fields = ['category','author','status']
+    filterset_fields = {'category':['exact','in'],'author':['exact'],'status':['exact']}
     search_fields = ['title','content']
     ordering_fields = ['published_date']
     pagination_class = DefaultPagination
