b106744503de46caed95d7da42d743a8796ad1f6
Marzya <mnaq_1987@yahoo.com>
2025-11-13 18:29:39 +0330

    Customizing the JWT structure
    Customizing the JWT structure

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.password_validation import validate_password
 from django.core import exceptions
 from django.contrib.auth import authenticate
 from django.utils.translation import gettext_lazy as _
-
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 
@@ -74,3 +74,12 @@ class CustomAuthTokenSerializer(serializers.Serializer):
 
         attrs['user'] = user
         return attrs
+    
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        validated_data = super().validate(attrs)
+        validated_data['email'] = self.user.email
+        validated_data['user_id'] = self.user.id
+        return validated_data
