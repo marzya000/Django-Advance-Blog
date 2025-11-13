85ba3a29affb319ff1505339d27cba2d72ec6ef8
Marzya <mnaq_1987@yahoo.com>
2025-11-13 15:10:02 +0330

    Implementing JWT with SimpleJWT
    Implementing JWT with SimpleJWT

--- core/core/settings.py
+++ core/core/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'django_filters',
     'drf_yasg',
-    
+    'rest_framework_simplejwt',    
 ]
 
 MIDDLEWARE = [
@@ -159,6 +159,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
 
