2fe99c4802cb117808163cc61bba5cd5acfa91ff
Marzya <mnaq_1987@yahoo.com>
2025-12-24 13:08:27 +0330

    Using Decorator to perform caching
    Using Decorator to perform caching

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 import time
 from .tasks import sendEmail
+import requests
 # Create your views here.
 
 def send_email(request):
     sendEmail.delay()
     return HttpResponse("<h1>Done Sending</h1>")
+
+
+
+@cache_page(60)
+def test(request):
+    response = requests.get("https://c393a699-bd6a-4c5c-b140-f435b616bfa1.mock.pstmn.io/test/delay/5")
+    return JsonResponse(response.json())
+
+
+
+@cache_page(60*20)
+def weather_view(request,city):  
+    response = requests.get("https://api.openweathermap.org/data/2.5/weather",params = {
+        "q": city,
+        "appid": "399871d262777e04375a1aea5f67f442",
+        "units": "metric",
+        "lang": "fa"
+        },timeout=5)
+    return JsonResponse(response.json())
+   
+    
--- core/core/settings.py
+++ core/core/settings.py
@@ -193,3 +193,14 @@ CORS_ALLOWED_ORIGINS = [
 
 CELERY_BROKER_URL = 'redis://redis:6379/1'
 
+# caching configs
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/3",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
